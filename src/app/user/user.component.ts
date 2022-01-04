15787e0644f1bbbbb0e500ee7174b7d403758ca3
Vadim Kosenkov <kosenkov@itspartner.net>
2022-01-04 13:44:06 +0300

    dynamically changing element properties

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.scss"],
 })
 export class UserComponent implements OnInit {
+  //new variable
+  public x = "congratulation";
+  //new variable
+  public user = {
+    name: "Vadim",
+  };
+  //new style variable
+  public myColor = "color-dark";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    // changing the value of a variable myColor
+    setTimeout(() => {
+      this.myColor = "color-light";
+      setTimeout(() => {
+        this.myColor = "color-bright";
+      }, 2000);
+    }, 2000);
   }
 
+  ngOnInit(): void {}
 }
