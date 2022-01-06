88ce5c3f3fead09c8367f1d04f258b508a98e443
Vadim Kosenkov <kosenkov@itspartner.net>
2022-01-06 20:07:30 +0300

    test commit

--- src/app/user/user-card/user-card.component.ts
+++ src/app/user/user-card/user-card.component.ts
@@ -6,14 +6,9 @@ import { Component, Input, OnInit } from "@angular/core";
   styleUrls: ["./user-card.component.scss"],
 })
 export class UserCardComponent implements OnInit {
-  // new decorator Input with import from "@angular/core"
   @Input() user: any;
 
-  constructor() {
-    console.log(this.user); // undefined
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-    console.log(this.user); // correct value
-  }
+  ngOnInit(): void {}
 }
