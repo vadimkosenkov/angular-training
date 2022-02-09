cbe293275127049e73adcdeb4239614db83e2edf
Vadim Kosenkov <kosenkov@itspartner.net>
2022-02-09 17:24:21 +0300

    working with directive,ComponentFactoryResolver, ViewContainerRef

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from "@angular/core";
+import {
+  Component,
+  ComponentFactoryResolver,
+  OnInit,
+  ViewContainerRef,
+} from "@angular/core";
+import { DynamicItemComponent } from "./dynamic-item/dynamic-item.component";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "angular-training";
+
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      const componentFactory =
+        this.componentFactoryResolver.resolveComponentFactory(
+          DynamicItemComponent
+        );
+      const componentRef =
+        this.viewContainerRef.createComponent(componentFactory);
+    }, 3000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UserComponent } from './user/user.component';
-import { UserCardComponent } from './user/user-card/user-card.component';
-import { ItemComponent } from './item/item.component';
-import { ColoryDirective } from './colory.directive';
-import { DelayDirective } from './delay.directive';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { UserComponent } from "./user/user.component";
+import { UserCardComponent } from "./user/user-card/user-card.component";
+import { ItemComponent } from "./item/item.component";
+import { ColoryDirective } from "./colory.directive";
+import { DelayDirective } from "./delay.directive";
+import { DynamicItemComponent } from "./dynamic-item/dynamic-item.component";
 
 @NgModule({
   declarations: [
@@ -17,14 +18,12 @@ import { DelayDirective } from './delay.directive';
     UserCardComponent,
     ItemComponent,
     ColoryDirective,
-    DelayDirective
+    DelayDirective,
+    DynamicItemComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  entryComponents: [DynamicItemComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
