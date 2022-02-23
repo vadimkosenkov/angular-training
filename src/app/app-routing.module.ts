c792360386988a37c8dab1ed563b3ab60f8cb671
Vadim Kosenkov <kosenkov@itspartner.net>
2022-02-23 15:16:08 +0300

    removed all content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
-import { ProfileComponent } from "./profile/profile.component";
-import { SettingsComponent } from "./settings/settings.component";
-import { UserComponent } from "./user/user.component";
-import { UsersComponent } from "./users/users.component";
 
-const routes: Routes = [
-  { path: "home", component: HomeComponent },
-  { path: "users", component: UsersComponent },
-  {
-    path: "users/:userId",
-    component: UserComponent,
-    children: [
-      { path: "profile", component: ProfileComponent },
-      { path: "settings", component: SettingsComponent },
-    ],
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,9 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { UserComponent } from './user/user.component';
-import { SettingsComponent } from './settings/settings.component';
-import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, UsersComponent, UserComponent, SettingsComponent, ProfileComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent],
